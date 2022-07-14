b4e884f9694b3c3f9e5c6098ac1bf8f97fcf0f1f
Asad Mahmood <asadmhmd1234@gmail.com>
2022-07-15 02:46:51 +0530

    Implement passport authentication and style the pages

---
+++ app.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const createError = require("http-errors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+const User = require("./models/user");
+
+// Set up database connection
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const app = express();
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      let user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(
+  session({ secret: "mickeymouse", resave: false, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.use("/", indexRouter);
+
+app.use((req, rest, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Listening to commands");
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,71 @@
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+const passport = require("passport");
 function indexGet(req, res, next) {}
 
 function loginGet(req, res, next) {
   res.render("index/login", { title: "Clubhouse - Log in" });
 }
 
-function loginPost(req, res, next) {}
+let loginPost = [
+  body("username", "Username should not be empty")
+    .isLength({ min: 1 })
+    .blacklist("<>"),
+  body("password", "Password should not be empty")
+    .isLength({ min: 1 })
+    .blacklist("<>"),
+  passport.authenticate("local", {
+    failureRedirect: "/log-in",
+  }),
+  (req, res, next) => {
+    console.log(req.user);
+    res.render("index/login", { title: "Clubhouse - Log In" });
+  },
+];
 
 function signupGet(req, res, next) {
   res.render("index/signup", { title: "Clubhouse - Sign up" });
 }
 
-function signupPost(req, res, next) {}
+let signupPost = [
+  body("firstname", "First Name should not be empty")
+    .isLength({ min: 1 })
+    .blacklist("<>"),
+  body("lastname", "Last Name should not be empty")
+    .isLength({ min: 1 })
+    .blacklist("<>"),
+  body("username", "Username should not be empty")
+    .isLength({ min: 1 })
+    .blacklist("<>"),
+  body("password", "Password should not be empty")
+    .isLength({ min: 1 })
+    .blacklist("<>"),
+  body("cnfpassword", "Passwords should match").custom(
+    (value, { req }) => value === req.body.password
+  ),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("index/signup", {
+        title: "Clubhouse - Sign Up",
+        error: errors.errors[0],
+      });
+    }
+    bcrypt.hash(req.body.password, 12, async (err, hash) => {
+      if (err) return next(err);
+      const newUser = new User({
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        username: req.body.username,
+        password: hash,
+      });
+      await newUser.save();
+    });
+    res.render("index/login", { title: "Clubhouse - Log In" });
+  },
+];
 
 function logoutGet(req, res, next) {}
 
